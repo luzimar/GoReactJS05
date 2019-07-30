bdbe389547deb250ecf1cca21f8ffcaf8bb77b0c
luzimar <lzm.spfc@hotmail.com>
2019-07-29 21:12:10 -0300

    Concluído mais um projeto em reactjs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,75 @@
-import React, { useState } from 'react';
-
-import { Container, Form, SubmitButton } from './styles';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import Container from '../../components/Container';
+import { Form, SubmitButton, Error, List } from './styles';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Main() {
-  const [repository, setRepository] = useState();
+  const [repository, setRepository] = useState('');
+  const [repositories, setRepositories] = useState(
+    JSON.parse(localStorage.getItem('repositories')) || []
+  );
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('repositories', JSON.stringify(repositories));
+  }, [repositories]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await api.get(`/repos/${repository}`);
+      const data = {
+        name: response.data.full_name,
+      };
+      setRepositories([...repositories, data]);
+      setRepository('');
+    } catch (error) {
+      setError('Repositório não encontrado');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Container>
       <h1>
         <FaGithubAlt />
         Repositórios
       </h1>
-      <Form onSubmit={() => alert(repository)}>
+      <Form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="adicionar repositório"
           value={repository}
-          onChange={e => setRepository(e.target.value)}
+          onChange={e => {
+            setRepository(e.target.value);
+            setError('');
+          }}
         />
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
+        <SubmitButton disabled={loading || repository === ''} loading={loading}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
         </SubmitButton>
       </Form>
+      <Error>{error}</Error>
+
+      <List>
+        {repositories.map(repository => (
+          <li key={repository.name}>
+            <span>{repository.name}</span>
+            <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+              Detalhes
+            </Link>
+          </li>
+        ))}
+      </List>
     </Container>
   );
 }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList } from './styles';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default function Repository({ match }) {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const repoName = decodeURIComponent(match.params.repository);
+  useEffect(() => {
+    async function fetchData() {
+      const [responseRepository, responseIssues] = await Promise.all([
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepository(responseRepository.data);
+      setIssues(responseIssues.data);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loading>Carregando</Loading>
+      ) : (
+        <Container>
+          <Owner>
+            <Link to="/">Voltar aos repositórios</Link>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <h1>{repository.name}</h1>
+            <p>{repository.description}</p>
+          </Owner>
+
+          <IssueList>
+            {issues.map(issue => (
+              <li key={String(issue.id)}>
+                <img src={issue.user.avatar_url} alt={issue.user.login} />
+                <div>
+                  <strong>
+                    <a href={issue.html_url} target="blank">
+                      {issue.title}
+                    </a>
+                    {issue.labels.map(label => (
+                      <span key={String(label.id)}>{label.name}</span>
+                    ))}
+                  </strong>
+                  <p>{issue.user.login}</p>
+                </div>
+              </li>
+            ))}
+          </IssueList>
+        </Container>
+      )}
+    </>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
